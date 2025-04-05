482820e8e2d8e027160a61f6ef85f482b0d7ed12
granttnarg <48766845+granttnarg@users.noreply.github.com>
2025-04-05 21:01:46 +0200

    extend job creator to add shifts

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :job do
     title { "Job_title_#{rand(1000)}" }
-    hourly_salary { 9.90 }
+    hourly_salary { 20 }
     association :company
     creator { association :user, admin: true, company: company }
 
@@ -9,6 +9,11 @@ FactoryBot.define do
       if job.languages.empty?
         english = Language.find_or_create_by(name: 'English', code: "en-#{Time.now.to_i}")
         job.languages << english unless job.languages.include?(english)
+      end
+
+      if job.shifts.empty?
+        shift = Shift.create(start_time: Time.current.beginning_of_hour + 1.hour, end_time: Time.current.beginning_of_hour + 2.hour)
+        job.shifts << shift
       end
     end
   end
