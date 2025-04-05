8a3199278ed87377c81db323421f4dc511ca99c5
granttnarg <48766845+granttnarg@users.noreply.github.com>
2025-04-06 01:12:00 +0200

    update seed file for more meaningful development data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# This File is just for data in Development.
+# Please note that we have a rake seeding task for Languages on Production. see lib/tasks/seed_languages.rake
+
 Job.destroy_all
 User.destroy_all
 Company.destroy_all
+Shift.destroy_all
+JobApplication.destroy_all
 
 puts "All DB data destroyed"
 
@@ -22,9 +18,45 @@ company = Company.create!(name: "Big Tech")
 admin.company = company
 admin.save
 
-languages = [ Language.find_or_create_by!(name: "English", code: 'en'), Language.find_or_create_by!(name: "German", code: 'de') ]
+languages = [
+  Language.find_or_create_by!(name: "English-Seed", code: 'en_seed'),
+  Language.find_or_create_by!(name: "German-Seed", code: 'de_seed')
+]
 
-Job.create!(title: "Senior Dev", hourly_salary: 50, company:, user_id: admin.id, languages:)
-Job.create!(title: "Jnr Dev", hourly_salary: 1, company:, user_id: admin.id, languages:)
+
+job1 = Job.new(title: "Senior Dev", hourly_salary: 50, company:, user_id: admin.id)
+job1.languages = languages
+job1.shifts.build(
+  start_time: Time.current.beginning_of_day + 9.hours,
+  end_time: Time.current.beginning_of_day + 17.hours
+)
+job1.save!
+
+2.times do |i|
+  Shift.create!(
+    job: job1,
+    start_time: Time.current.beginning_of_day + (i + 1).days + 9.hours,
+    end_time: Time.current.beginning_of_day + (i + 1).days + 17.hours
+  )
+end
+
+job2 = Job.new(title: "Jnr Dev", hourly_salary: 1, company:, user_id: admin.id)
+job2.languages = languages
+job2.shifts.build(
+  start_time: Time.current.beginning_of_day + 9.hours,
+  end_time: Time.current.beginning_of_day + 17.hours
+)
+job2.save!
+
+2.times do |i|
+  Shift.create!(
+    job: job2,
+    start_time: Time.current.beginning_of_day + (i + 1).days + 9.hours,
+    end_time: Time.current.beginning_of_day + (i + 1).days + 17.hours
+  )
+end
+
+JobApplication.create!(user: applicant_user, job: job1, status: "pending")
+JobApplication.create!(user: applicant_user, job: job2, status: "rejected")
 
 puts "Seeds Created"
