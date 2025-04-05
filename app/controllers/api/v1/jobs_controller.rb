abb0b39cc9799c86b7550e6a4842e8ff1bffdc19
granttnarg <48766845+granttnarg@users.noreply.github.com>
2025-04-05 23:58:49 +0200

    Add basic search route

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,11 +1,26 @@
 class Api::V1::JobsController < Api::V1::BaseController
-  skip_before_action :authenticate_user, only: [ :index, :show ]
+  skip_before_action :authenticate_user, only: [ :index, :show, :search ]
+
+  def initialize
+    super
+    @job_searcher = Jobs::Search::Service
+  end
 
   def index
-    @jobs = Job.all
+    @jobs = Job.includes(:languages).order(created_at: :desc).limit(10)
   end
 
   def show
     @job = Job.find(params[:id])
+  end
+
+  def search
+    @jobs = @job_searcher.search(search_params)
+  end
+
+  private
+
+  def search_params
+    params.permit(:title, language_codes: [],)
   end
 end
